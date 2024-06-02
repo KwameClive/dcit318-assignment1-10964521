30ab31f011fe00fe796067bd4d52ee9027dd55d9
KwameClive <102954657+KwameClive@users.noreply.github.com>
2024-06-02 06:10:31 -0700

    Ticket Price Calculator

---
+++ Ticket Price Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class TicketPriceCalculator
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your age: ");
+
+        // Read user input and convert to integer
+        int age = int.Parse(Console.ReadLine());
+
+        double ticketPrice;
+
+        if (age >= 65 || age <= 12)
+        {
+            ticketPrice = 7.0; // Discounted price
+        }
+        else
+        {
+            ticketPrice = 10.0; // Regular price
+        }
+
+        Console.WriteLine($"Your ticket price is: GHC{ticketPrice}");
+    }
+}
